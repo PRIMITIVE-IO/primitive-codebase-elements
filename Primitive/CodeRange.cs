58ae157b0a4f19cd96c38b84a4148aee02089afd
John <john@primitive.io>
2020-09-17 09:24:58 -0700

    Initial commit

---
+++ Primitive/CodeRange.cs
@@ -0,0 +1,37 @@
+﻿namespace PrimitiveCodebaseElements.Primitive
+{
+    public class CodeRange
+    {
+        public readonly int Start;
+        public readonly int End;
+
+        public CodeRange(
+            int startPosition,
+            int endPosition)
+        {
+            Start = startPosition;
+            End = endPosition;
+        }
+
+        public override string ToString() => $"(Start={Start}, End={End})";
+    }
+
+    public class CodeRangeWithReference
+    {
+        public readonly int Start;
+        public readonly int End;
+        public readonly CodebaseElementName Reference;
+
+        public CodeRangeWithReference(
+            CodeRange codeRange,
+            CodebaseElementName reference)
+        {
+            Start = codeRange.Start;
+            End = codeRange.End;
+            Reference = reference;
+        }
+
+        public override string ToString() =>
+            $"{Reference.FullyQualified}: (Start={Start}, End={End})";
+    }
+}
---
+++ Primitive/TextSpanWithReference.cs
@@ -0,0 +1,69 @@
+﻿using System.Collections.Generic;
+using System.Text;
+
+namespace PrimitiveCodebaseElements.Primitive
+{
+    public class TextSpanWithReference
+    {
+        public int Start { get; }
+        public int End { get; }
+        public readonly CodebaseElementName Reference;
+
+        public TextSpanWithReference(int start, int end, CodebaseElementName reference)
+        {
+            Start = start;
+            End = end;
+            Reference = reference;
+        }
+
+        public TextSpanWithReference AdjustedBy(int offset) =>
+            new TextSpanWithReference(Start + offset, End + offset, Reference);
+
+        public override string ToString() =>
+            $"{Reference.FullyQualified}: (Start={Start}, End={End})";
+    }
+
+    public class Insertion
+    {
+        readonly int Position;
+        readonly string Text;
+
+        public Insertion(int position, string text)
+        {
+            Position = position;
+            Text = text;
+        }
+
+        public static string TextWithInsertions(
+            string text,
+            IEnumerable<Insertion> insertions)
+        {
+            if (string.IsNullOrEmpty(text)) return "";
+            
+            StringBuilder builder = new StringBuilder();
+
+            int lastInsertionPosition = 0;
+
+            foreach (Insertion insertion in insertions)
+            {
+                // All insertion positions are based on the original text positions, so we should always get substrings
+                // of the original text.
+                builder.Append(
+                    text.Substring(
+                        lastInsertionPosition,
+                        insertion.Position - lastInsertionPosition));
+
+                builder.Append(insertion.Text);
+
+                lastInsertionPosition = insertion.Position;
+            }
+
+            builder.Append(text.Substring(lastInsertionPosition));
+
+            return builder.ToString();
+        }
+
+        public override string ToString() =>
+            $"(Position={Position}, Text={Text})";
+    }
+}
