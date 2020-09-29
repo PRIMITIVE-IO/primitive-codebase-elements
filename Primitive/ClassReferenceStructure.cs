714d381a48e4a874f7a1a9c692f7f47bef297130
John <john@primitive.io>
2020-09-28 22:23:42 -0600

    add class ref structure

---
+++ Primitive/ClassReferenceStructure.cs
@@ -0,0 +1,67 @@
+using System.Collections.Generic;
+
+namespace PrimitiveCodebaseElements.Primitive
+{
+    public class ClassReferenceStructure
+    {
+        public readonly ClassName ClassName;
+        public readonly ClassInfo ClassInfo;
+        public readonly List<MethodReferenceStructure> Methods = new List<MethodReferenceStructure>();
+        
+        public readonly List<int> SolvedNodes = new List<int>();
+        
+        public readonly List<CodeRangeWithReference> OutboundUsageLinks = new List<CodeRangeWithReference>();
+        public readonly List<CodeReferenceEndpoint> ReferencesFromThis = new List<CodeReferenceEndpoint>();
+
+        public ClassReferenceStructure(ClassInfo classInfo)
+        {
+            ClassName = classInfo.className;
+            foreach (MethodInfo methodInfo in classInfo.Methods)
+            {
+                Methods.Add(new MethodReferenceStructure(methodInfo));
+            }
+        }
+        
+        public ClassReferenceStructure(ClassInfo classInfo, List<MethodReferenceStructure> MethodNodes)
+        {
+            ClassName = classInfo.className;
+            ClassInfo = classInfo;
+            Methods = MethodNodes;
+        }
+    }
+
+    public class MethodReferenceStructure
+    {
+        public readonly MethodName MethodName;
+        public readonly MethodInfo MethodInfo;
+        
+        public readonly List<int> SolvedCsNodes = new List<int>();
+        
+        public readonly List<object> SolvedJavaNodes = new List<object>();
+        public readonly Dictionary<object, object> DiscoveredNodes = new Dictionary<object, object>();
+        
+        public readonly List<CodeRangeWithReference> OutboundUsageLinks = new List<CodeRangeWithReference>();
+        public readonly List<CodeReferenceEndpoint> ReferencesFromThis = new List<CodeReferenceEndpoint>();
+
+        public readonly string MethodString;
+        
+        public MethodReferenceStructure(MethodInfo methodInfo)
+        {
+            MethodName = methodInfo.MethodName;
+            MethodInfo = methodInfo;
+        }
+
+        public MethodReferenceStructure(
+            MethodInfo methodInfo, 
+            Dictionary<object, object> discoveredNodes,
+            string methodString)
+        {
+            MethodName = methodInfo.MethodName;
+            MethodInfo = methodInfo;
+            DiscoveredNodes = discoveredNodes;
+            MethodString = methodString;
+        }
+    }
+    
+    
+}
