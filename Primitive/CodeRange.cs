6cd5e8d3b133e13b53cca315ae8a2ce20a3c4bcf
Artem <artem@primitive.io>
2022-01-04 17:36:03 +0200

    line and column indexing added

--- Primitive/CodeRange.cs
+++ Primitive/CodeRange.cs
@@ -7,13 +7,25 @@ namespace PrimitiveCodebaseElements.Primitive
     {
         public readonly int Start;
         public readonly int End;
+        public readonly int StartLine;
+        public readonly int EndLine;
+        public readonly int StartColumn;
+        public readonly int EndColumn;
 
         public CodeRange(
             int startPosition,
-            int endPosition)
+            int endPosition,
+            int startLine,
+            int startColumn,
+            int endLine,
+            int endColumn)
         {
             Start = startPosition;
             End = endPosition;
+            StartLine = startLine;
+            EndLine = endLine;
+            StartColumn = startColumn;
+            EndColumn = endColumn;
         }
     }
 
