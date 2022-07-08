64b5cfbfb30098e16443c9f865a44581a637af21
Max Farsikov <farsikovmax@gmail.com>
2022-07-08 15:35:32 +0300

    Fault-tolerant TableSet -> DTO converter
    each level of:
    * tableset
      * file
        * class
          * class ref
          * method
            * method ref
          * field
    
    wrapped into try-catch, logs an error with a stacktrace, returns default value, which gets filtered out on upper level.
    
    This needs to not break Front-end during reading DB

--- Primitive/db/converter/TableSetToDtoConverter.cs
+++ Primitive/db/converter/TableSetToDtoConverter.cs
@@ -1,7 +1,9 @@
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using JetBrains.Annotations;
 using PrimitiveCodebaseElements.Primitive.dto;
+using PrimitiveLogger;
 
 namespace PrimitiveCodebaseElements.Primitive.db.converter
 {
@@ -24,100 +26,140 @@ namespace PrimitiveCodebaseElements.Primitive.db.converter
 
         public static List<FileDto> ToFileDto(TableSet tableSet)
         {
-            ILookup<int, DbClass> classesByFileId = tableSet.Classes.ToLookup(it => it.ParentId);
+            try
+            {
+                ILookup<int, DbClass> classesByFileId = tableSet.Classes.ToLookup(it => it.ParentId);
 
-            Dictionary<int, DbClass> classesById = tableSet.Classes.ToDictionary(it => it.Id);
-            ILookup<int, DbMethod> methodByClassId = tableSet.Methods.ToLookup(it => it.ParentId);
+                Dictionary<int, DbClass> classesById = tableSet.Classes.ToDictionary(it => it.Id);
+                ILookup<int, DbMethod> methodByClassId = tableSet.Methods.ToLookup(it => it.ParentId);
 
-            ILookup<int, DbField> fieldByClassId = tableSet.Fields.ToLookup(it => it.ParentId);
-            Dictionary<int, DbType> types = tableSet.Types.ToDictionary(it => it.Id);
-            ILookup<int, DbArgument> argsByMethodId = tableSet.Arguments.ToLookup(it => it.MethodId);
-            Dictionary<int, string> methodSignaturesById = tableSet.Methods.ToDictionary(it => it.Id,
-                it => Signature(it, classesById, argsByMethodId[it.Id].ToList(), types));
+                ILookup<int, DbField> fieldByClassId = tableSet.Fields.ToLookup(it => it.ParentId);
+                Dictionary<int, DbType> types = tableSet.Types.ToDictionary(it => it.Id);
+                ILookup<int, DbArgument> argsByMethodId = tableSet.Arguments.ToLookup(it => it.MethodId);
+                Dictionary<int, string> methodSignaturesById = tableSet.Methods.ToDictionary(it => it.Id,
+                    it => Signature(it, classesById, argsByMethodId[it.Id].ToList(), types));
 
-            Dictionary<int, DbSourceIndex> methodIndices = tableSet.SourceIndices
-                .Where(it => it.Type == "METHOD")
-                .ToDictionary(it => it.ElementId);
+                Dictionary<int, DbSourceIndex> methodIndices = tableSet.SourceIndices
+                    .Where(it => it.Type == "METHOD")
+                    .ToDictionary(it => it.ElementId);
 
-            Dictionary<int, DbSourceIndex> fieldIndices = tableSet.SourceIndices
-                .Where(it => it.Type == "FIELD")
-                .ToDictionary(it => it.ElementId);
+                Dictionary<int, DbSourceIndex> fieldIndices = tableSet.SourceIndices
+                    .Where(it => it.Type == "FIELD")
+                    .ToDictionary(it => it.ElementId);
 
-            Dictionary<int, DbSourceIndex> classIndices = tableSet.SourceIndices
-                .Where(it => it.Type == "CLASS")
-                .ToDictionary(it => it.ElementId);
+                Dictionary<int, DbSourceIndex> classIndices = tableSet.SourceIndices
+                    .Where(it => it.Type == "CLASS")
+                    .ToDictionary(it => it.ElementId);
 
-            ILookup<int, DbClassReference> classReferencesByClassId =
-                tableSet.ClassReferences.ToLookup(it => it.FromId);
+                ILookup<int, DbClassReference> classReferencesByClassId =
+                    tableSet.ClassReferences.ToLookup(it => it.FromId);
 
-            ILookup<int, DbMethodReference> methodReferencesById = tableSet.MethodReferences.ToLookup(it => it.FromId);
+                ILookup<int, DbMethodReference> methodReferencesById =
+                    tableSet.MethodReferences.ToLookup(it => it.FromId);
 
-            return tableSet.Files.Select(dbFile =>
-                {
-                    List<ClassDto> classes = classesByFileId[dbFile.Id]
-                        .Select(dbClass =>
+                return tableSet.Files.SelectNotNull(dbFile =>
+                    {
+                        try
                         {
-                            List<MethodDto> methodDtos = methodByClassId[dbClass.Id].Select(it => ToMethodDto(
-                                    it,
-                                    argsByMethodId[it.Id].ToList(),
-                                    types,
-                                    methodIndices[it.Id],
-                                    dbClass,
-                                    methodReferencesById[it.Id].ToList(),
-                                    methodSignaturesById
-                                ))
+                            List<ClassDto> classes = classesByFileId[dbFile.Id]
+                                .SelectNotNull(dbClass =>
+                                {
+                                    try
+                                    {
+                                        List<MethodDto> methodDtos = methodByClassId[dbClass.Id].SelectNotNull(it =>
+                                                ToMethodDto(
+                                                    it,
+                                                    argsByMethodId[it.Id].ToList(),
+                                                    types,
+                                                    methodIndices[it.Id],
+                                                    dbClass,
+                                                    methodReferencesById[it.Id].ToList(),
+                                                    methodSignaturesById
+                                                ))
+                                            .ToList();
+
+                                        List<FieldDto> fieldDtos = fieldByClassId[dbClass.Id].SelectNotNull(it =>
+                                                ToFieldDto(
+                                                    it,
+                                                    types,
+                                                    fieldIndices[it.Id]
+                                                ))
+                                            .ToList();
+
+                                        List<ClassReferenceDto> classReferences = classReferencesByClassId[dbClass.Id]
+                                            .SelectNotNull(
+                                                it =>
+                                                {
+                                                    try
+                                                    {
+                                                        return new ClassReferenceDto(
+                                                            type: (CodeReferenceType)it.Type,
+                                                            fromFqn: dbClass.Fqn,
+                                                            toFqn: classesById[it.ToId].Fqn,
+                                                            startPosition: it.StartPosition,
+                                                            endPosition: it.EndPosition,
+                                                            startIdx: it.StartIdx,
+                                                            endIdx: it.EndIdx,
+                                                            codeRange: CodeRange(it)
+                                                        );
+                                                    }
+                                                    catch (Exception ex)
+                                                    {
+                                                        Logger.Instance().Error($"Cannot create ClassReferenceDto id: {it.Id}", ex);
+                                                        return null;
+                                                    }
+                                                }
+                                            )
+                                            .ToList();
+
+                                        DbSourceIndex classIndex = classIndices[dbClass.Id];
+                                        return new ClassDto(
+                                            dbFile.Path,
+                                            packageName: PackageName(dbClass.Fqn),
+                                            name: ClassName(dbClass.Fqn),
+                                            fullyQualifiedName: dbClass.Fqn,
+                                            methods: methodDtos,
+                                            fields: fieldDtos,
+                                            modifier: (AccessFlags)dbClass.AccessFlags,
+                                            startIdx: classIndex.StartIdx,
+                                            endIdx: classIndex.EndIdx,
+                                            codeRange: CodeRange(classIndex),
+                                            header: string.Empty,
+                                            referencesFromThis: classReferences,
+                                            parentClassFqn: ParenClassFqn(dbClass.Fqn)
+                                        );
+                                    }
+                                    catch (Exception ex)
+                                    {
+                                        Logger.Instance().Error($"Cannot crate ClassDto for {dbClass.Fqn}", ex);
+                                        return null;
+                                    }
+                                })
                                 .ToList();
 
-                            List<FieldDto> fieldDtos = fieldByClassId[dbClass.Id].Select(it => ToFieldDto(
-                                    it,
-                                    types,
-                                    fieldIndices[it.Id]
-                                ))
-                                .ToList();
+                            bool isTest = classesByFileId[dbFile.Id].Any(it => it.IsTestClass == 1);
 
-                            List<ClassReferenceDto> classReferences = classReferencesByClassId[dbClass.Id].Select(it =>
-                                    new ClassReferenceDto(
-                                        type: (CodeReferenceType)it.Type,
-                                        fromFqn: dbClass.Fqn,
-                                        toFqn: classesById[it.ToId].Fqn,
-                                        startPosition: it.StartPosition,
-                                        endPosition: it.EndPosition,
-                                        startIdx: it.StartIdx,
-                                        endIdx: it.EndIdx,
-                                        codeRange: CodeRange(it)
-                                    ))
-                                .ToList();
-
-                            DbSourceIndex classIndex = classIndices[dbClass.Id];
-                            return new ClassDto(
-                                dbFile.Path,
-                                packageName: PackageName(dbClass.Fqn),
-                                name: ClassName(dbClass.Fqn),
-                                fullyQualifiedName: dbClass.Fqn,
-                                methods: methodDtos,
-                                fields: fieldDtos,
-                                modifier: (AccessFlags)dbClass.AccessFlags,
-                                startIdx: classIndex.StartIdx,
-                                endIdx: classIndex.EndIdx,
-                                codeRange: CodeRange(classIndex),
-                                header: string.Empty,
-                                referencesFromThis: classReferences,
-                                parentClassFqn: ParenClassFqn(dbClass.Fqn)
+                            return new FileDto(
+                                text: dbFile.SourceText,
+                                path: dbFile.Path,
+                                isTest: isTest,
+                                classes: classes,
+                                language: (SourceCodeLanguage)dbFile.Language
                             );
-                        })
-                        .ToList();
-
-                    bool isTest = classesByFileId[dbFile.Id].Any(it => it.IsTestClass == 1);
-
-                    return new FileDto(
-                        text: dbFile.SourceText,
-                        path: dbFile.Path,
-                        isTest: isTest,
-                        classes: classes,
-                        language: (SourceCodeLanguage)dbFile.Language
-                    );
-                })
-                .ToList();
+                        }
+                        catch (Exception ex)
+                        {
+                            Logger.Instance().Error($"Cannot create FileDto for {dbFile.Path}", ex);
+                            return null;
+                        }
+                    })
+                    .ToList();
+            }
+            catch (Exception ex)
+            {
+                Logger.Instance().Error("Cannot convert TableSet to file DTOs", ex);
+                return new List<FileDto>();
+            }
         }
 
         static string Signature(
@@ -151,23 +193,33 @@ namespace PrimitiveCodebaseElements.Primitive.db.converter
             return fqn.Contains('$') ? fqn.SubstringBeforeLast("$") : null;
         }
 
+        [CanBeNull]
         static FieldDto ToFieldDto(
             DbField field,
             IReadOnlyDictionary<int, DbType> types,
             DbSourceIndex index
         )
         {
-            return new FieldDto(
-                field.Name,
-                type: types[field.TypeId].Signature,
-                (AccessFlags)field.AccessFlags,
-                sourceCode: string.Empty,
-                startIdx: index.StartIdx,
-                endIdx: index.EndIdx,
-                codeRange: CodeRange(index)
-            );
+            try
+            {
+                return new FieldDto(
+                    field.Name,
+                    type: types[field.TypeId].Signature,
+                    (AccessFlags)field.AccessFlags,
+                    sourceCode: string.Empty,
+                    startIdx: index.StartIdx,
+                    endIdx: index.EndIdx,
+                    codeRange: CodeRange(index)
+                );
+            }
+            catch (Exception ex)
+            {
+                Logger.Instance().Error($"Cannot create FieldDto, id: {field.Id}", ex);
+                return null;
+            }
         }
 
+        [CanBeNull]
         static MethodDto ToMethodDto(
             DbMethod method,
             List<DbArgument> arguments,
@@ -177,41 +229,60 @@ namespace PrimitiveCodebaseElements.Primitive.db.converter
             List<DbMethodReference> methodReferences,
             Dictionary<int, string> methodSignaturesById)
         {
-            List<ArgumentDto> args = arguments
-                .Select(it => new ArgumentDto(
-                    index: it.ArgIndex,
-                    name: it.Name,
-                    type: types[it.TypeId].Signature
-                ))
-                .ToList();
+            try
+            {
+                List<ArgumentDto> args = arguments
+                    .Select(it => new ArgumentDto(
+                        index: it.ArgIndex,
+                        name: it.Name,
+                        type: types[it.TypeId].Signature
+                    ))
+                    .ToList();
 
-            string methodSignature = MethodDto.MethodSignature(cls.Fqn, method.Name, args);
+                string methodSignature = MethodDto.MethodSignature(cls.Fqn, method.Name, args);
 
-            List<MethodReferenceDto> methodReferenceDtos = methodReferences.Select(it => new MethodReferenceDto(
-                    type: (CodeReferenceType)it.Type,
-                    fromMethodSignature: methodSignature,
-                    toMethodSignature: methodSignaturesById[it.ToId],
-                    startPosition: it.StartPosition,
-                    endPosition: it.EndPosition,
-                    startIdx: it.StartIdx,
-                    endIdx: it.EndIdx,
-                    codeRange: CodeRange(it)
-                ))
-                .ToList();
+                List<MethodReferenceDto> methodReferenceDtos = methodReferences.SelectNotNull(it =>
+                    {
+                        try
+                        {
+                            return new MethodReferenceDto(
+                                type: (CodeReferenceType)it.Type,
+                                fromMethodSignature: methodSignature,
+                                toMethodSignature: methodSignaturesById[it.ToId],
+                                startPosition: it.StartPosition,
+                                endPosition: it.EndPosition,
+                                startIdx: it.StartIdx,
+                                endIdx: it.EndIdx,
+                                codeRange: CodeRange(it)
+                            );
+                        }
+                        catch (Exception ex)
+                        {
+                            Logger.Instance().Error($"Cannot create MethodReferenceDto, id: {it.Id}", ex);
+                            return null;
+                        }
+                    })
+                    .ToList();
 
-            return new MethodDto(
-                signature: methodSignature,
-                name: method.Name,
-                accFlag: (AccessFlags)method.AccessFlags,
-                arguments: args,
-                returnType: types[method.ReturnTypeId].Signature,
-                sourceCode: string.Empty,
-                startIdx: dbSourceIndex.StartIdx,
-                endIdx: dbSourceIndex.EndIdx,
-                codeRange: CodeRange(dbSourceIndex),
-                methodReferences: methodReferenceDtos,
-                cyclomaticScore: method.CyclomaticScore
-            );
+                return new MethodDto(
+                    signature: methodSignature,
+                    name: method.Name,
+                    accFlag: (AccessFlags)method.AccessFlags,
+                    arguments: args,
+                    returnType: types[method.ReturnTypeId].Signature,
+                    sourceCode: string.Empty,
+                    startIdx: dbSourceIndex.StartIdx,
+                    endIdx: dbSourceIndex.EndIdx,
+                    codeRange: CodeRange(dbSourceIndex),
+                    methodReferences: methodReferenceDtos,
+                    cyclomaticScore: method.CyclomaticScore
+                );
+            }
+            catch (Exception ex)
+            {
+                Logger.Instance().Error($"Cannot create MethodDto id: {method.Id}", ex);
+                return null;
+            }
         }
 
         [CanBeNull]
